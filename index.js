b1c927ea85a2b58456df04c9d953651b8d76e5ac
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 11:15:25 +0300

    write code to move the dodger when the left key is pressed

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ dodger.style.backgroundColor = "#FF69B4";
 
 // moving the element
 // moving up
-dodger.style.bottom = "100px";
+// dodger.style.bottom = "100px";
 
 // resetting the values
-dodger.style.left = "0px";
-dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+// dodger.style.bottom = "0px";
+
+// Demonstrate How to Move an Element in Response to a Browser Event
+// document.addEventListener("keydown", function (e) {
+//   console.log(e);
+// });
+
+// code to move the dodger left when the key is pressed:
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+  }
+});

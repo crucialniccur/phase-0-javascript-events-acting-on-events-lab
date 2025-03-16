e827ee5a5383c77ca33c5fcb2eaa41b86cb15160
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 11:02:46 +0300

    modify the dodgers position then reset it to the botttom left corner

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ console.log(dodger);
 
 // change dodger color
 dodger.style.backgroundColor = "#FF69B4";
+
+// moving the element
+// moving up
+dodger.style.bottom = "100px";
+
+// resetting the values
+dodger.style.left = "0px";
+dodger.style.bottom = "0px";

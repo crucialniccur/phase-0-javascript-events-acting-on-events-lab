54da3f0262d3647468df26529f30f716900064d4
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 11:46:24 +0300

    code the moveleft of the dodger

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+// const dodger = document.getElementById("dodger");
 
-console.log(dodger);
+// console.log(dodger);
 
 // change dodger color
-dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.backgroundColor = "#FF69B4";
+// console.log(dodger);
 
 // moving the element
 // moving up
@@ -20,11 +21,47 @@ dodger.style.backgroundColor = "#FF69B4";
 // });
 
 // code to move the dodger left when the key is pressed:
-document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowLeft") {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "ArrowLeft") {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+//     dodger.style.left = `${left - 1}px`;
+//     // console.log(event.key);
+//   }
+// });
+
+// Update an Element's Position on the Page Conditionally
+// -----------
+// function moveDodgerLeft() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px;`;
+//   }
+// }
+
+// // moveDodgerLeft();
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+//   }
+// });
+
+const dodger = document.getElementById("dodger");
+console.log(dodger);
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`;
-    // console.log(event.key);
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
   }
 });
